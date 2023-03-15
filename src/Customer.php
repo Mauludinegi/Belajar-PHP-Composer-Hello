c8bec2fa5a3feb75c05278f1fc5f70199ada4f78
Mauludin Egi <mauludinegi@gmail.com>
2023-03-15 14:50:06 +0700

    learn composer

---
+++ src/Customer.php
@@ -0,0 +1,17 @@
+<?php
+namespace ProgrammerGie\Belajar;
+/**
+ * Summary of Customer
+ */
+class Customer {
+    /**
+     * Summary of __construct
+     * @param string $name
+     */
+    public function __construct(private string $name) {
+
+    }
+    public function sayHello(string $name):string {
+        return "Hello $name, My name is $this->name";
+    }
+}
